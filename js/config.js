1a624d99ccc3254180bd7bd278ba03b4c02bc4d8
KULDEEP KUMAR VISHWAKARMA <104729596+kkv123@users.noreply.github.com>
2023-05-08 23:36:54 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoB4INfTwN5g7gfx8653Y2x7H5_bFRxlI",
+    authDomain: "ela-17-4-23.firebaseapp.com",
+    databaseURL: "https://ela-17-4-23-default-rtdb.firebaseio.com",
+    projectId: "ela-17-4-23",
+    storageBucket: "ela-17-4-23.appspot.com",
+    messagingSenderId: "1085188779486",
+    appId: "1:1085188779486:web:1e94acd8f29ffa5ab7814f",
+    measurementId: "G-CMZK01WMD1"
+};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA55P82NHGN4GBZaRefs8NkNUCBnbKSYr0",
+//     authDomain: "elitmus-122002-project.firebaseapp.com",
+//     databaseURL: "https://elitmus-122002-project-default-rtdb.firebaseio.com",
+//     projectId: "elitmus-122002-project",
+//     storageBucket: "elitmus-122002-project.appspot.com",
+//     messagingSenderId: "906008857551",
+//     appId: "1:906008857551:web:b8e03b99f341ed23cea0ba",
+//     measurementId: "G-Z0X8P7Z5YP"
+//   };
+firebase.initializeApp(firebaseConfig);
